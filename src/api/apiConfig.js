0fc5a2c00001e760f11d00facb7924af08f57354
LuckyAndrean_20075 <luckyand222@gmail.com>
2023-05-30 19:36:34 +0700

    push

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
-  baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "5fdae6bf243f3781c09dd2c93e209588",
+  baseUrl: `${process.env.REACT_APP_TMDB_BASE_URL}`,
+  apiKey: `${process.env.REACT_APP_TMDB_KEY}`,
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
